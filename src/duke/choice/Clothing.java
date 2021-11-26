8c05ca0788f7d6fe0361c842722213dc3d391791
Brian <brian.fly0623@gmail.com>
2021-11-26 17:30:22 -0300

    clothing new logic system

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -3,6 +3,12 @@ package duke.choice;
 public class Clothing {
 	String description;
 	double price;
-	String size = "M";
+	String size;
 
+	public void measure(int value) {
+		size = (value < 10) ? "S" : "M";
+	}
+	public String getSize() {
+		return size;
+	}
 }
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -16,7 +16,7 @@ public class ShopApp {
 		Clothing item1 = new Clothing();
 		Clothing item2 = new Clothing();
 		
-		Clothing[] items = {item1, item2};
+		Clothing[] items = {item1, item2, new Clothing(), new Clothing()};
 		
 		item1.description = "Blue jacket";
 		item1.price = 20.9;
@@ -24,6 +24,14 @@ public class ShopApp {
 		item2.description = "Orange T-Shirt";
 		item2.price = 10.5;
 		item2.size = "S";
+		
+		items[2].description = "Green Scarf";
+		items[2].price = 5.0;
+		items[2].size = "S";
+		
+		items[3].description = "Blue T-Shirt";
+		items[3].price = 5.0;
+		items[3].size = "S";
 		
 //		System.out.println("First item: " + item1.description + ", " + item1.price + " dolars and size " + item1.size);
 //		System.out.println("Second item: " + item2.description + ", " + item2.price + " dolars and size " + item2.size);
@@ -49,11 +57,19 @@ public class ShopApp {
 		for (Clothing item : items) {
 			if (c1.size.equals(item.size)) {
 				total += item.price;
-				System.out.println("Item: " + item.description + ", " + item.price + " dolars and size " + item.size);			
+				System.out.println("Item: " + item.description + ", " + item.price + " dolars and size " + item.size);
+				if (total > 15 ) {
+					break;
+				}
 			}
 		}
 
 		System.out.println("Total = " + total);
+		
+		Clothing jacket = new Clothing();
+		jacket.measure(12);
+		String size = jacket.getSize();
+		System.out.println(jacket.getSize());
 		
 	}
 }
