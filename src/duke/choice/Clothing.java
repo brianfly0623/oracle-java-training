fdcf2dbcaed5e59ec26b68843aeab463bc6d8e2a
Brian <brian.fly0623@gmail.com>
2021-12-02 16:15:52 -0300

    encapsulation

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -1,14 +1,38 @@
 package duke.choice;
 
 public class Clothing {
-	String description;
-	double price;
-	String size;
+
+		private String description;
+		private double price;
+		private String size;
 
 	public void measure(int value) {
 		size = (value < 10) ? "S" : "M";
 	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
 	public String getSize() {
 		return size;
 	}
+	
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	
 }
